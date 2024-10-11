ff34e3dda0ce5d945fbc8618942f088162e45d42
akishajae <akishajae.angeles@gmail.com>
2024-10-11 19:18:50 +0200

    bankAccount class added

--- src/ComBank/Bank/BankAccount.php
+++ src/ComBank/Bank/BankAccount.php
@@ -18,6 +18,80 @@ use ComBank\OverdraftStrategy\Contracts\OverdraftInterface;
 use ComBank\Support\Traits\AmountValidationTrait;
 use ComBank\Transactions\Contracts\BankTransactionInterface;
 
-class BankAccount
-{
+class BankAccount{
+
+    // Properties
+    private $balance;
+    private $status;
+    private $overdraft;
+
+    // Constructor
+    function __construct($balance, $status, $overdraft) {
+        $this->balance = $balance;
+        $this->status = $status;
+        $this->overdraft = $overdraft;
     }
+
+    // Methods
+    /**
+     * Get the value of balance
+     */ 
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    /**
+     * Set the value of balance
+     *
+     * @return  self
+     */ 
+    public function setBalance($balance)
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of status
+     */ 
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set the value of status
+     *
+     * @return  self
+     */ 
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of overdraft
+     */ 
+    public function getOverdraft()
+    {
+        return $this->overdraft;
+    }
+
+    /**
+     * Set the value of overdraft
+     *
+     * @return  self
+     */ 
+    public function setOverdraft($overdraft)
+    {
+        $this->overdraft = $overdraft;
+
+        return $this;
+    }
+}
+
+
